eef9c6a87c32bacaf35e8fe66bd43b0f7a34b63c
Alexander Urizar <urizaraj@gmail.com>
2018-04-03 18:58:21 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,38 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[description inventory]
 
+  def index
+    @products = Product.all
+  end
+
+  def description
+    render plain: @product.description
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(strong_params)
+    return render :new unless @product.save
+    redirect_to products_path
+  end
+
+  def inventory
+    render plain: (@product.inventory.zero? ? 'false' : 'true')
+  end
+
+  private
+
+  def strong_params
+    params.require(:product).permit(
+      :name,
+      :description
+    )
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
+
+  resources :products do
+    get 'description', on: :member
+    get 'inventory', on: :member
+  end
 end
